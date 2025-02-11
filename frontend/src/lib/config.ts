a17a3504ea1a4524ec61c5bb68a8b0f3948969b6
JNNJ <95310130+CodeName-Anti@users.noreply.github.com>
2025-02-11 14:45:57 +0100

    frontend: fix create post

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -4,5 +4,5 @@ import { version as envVersion } from "$app/environment";
 export const defaultHost = env.PUBLIC_DEFAULT_HOST ?? "localhost";
 export const apiBaseUrl = env.PUBLIC_API_BASE_URL ?? "http://localhost:5226";
 export const fallbackReturnUrl = env.PUBLIC_FALLBACK_RETURN_URL ?? "http://localhost:5173";
-export const withCredentials = !!env.PUBLIC_WITH_CREDENTIALS;
+export const withCredentials = env.PUBLIC_WITH_CREDENTIALS as unknown as boolean ?? true;
 export const version = envVersion ?? "0.0.0";
--- frontend/src/routes/createPost/+page.svelte
+++ frontend/src/routes/createPost/+page.svelte
@@ -12,20 +12,17 @@
 		if (selectedFile === undefined) return;
 
 		const response = await apiClient.post.v1PostCreate({
-			CreatorId: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
 			Title: title,
 			Message: message,
 			Image: selectedFile
 		});
-
-		if (!response.ok) return;
 
 		await goto("/");
 	}
 </script>
 
 <div class="flex items-center justify-center">
-	<div class="w-[50rem]">
+	<div class="w-full">
 		<div class="flex flex-col gap-4">
 			<input bind:value={title} class="bg-primary" maxlength="128" placeholder="Title" type="text" />
 			<textarea bind:value={message} class="resize-none bg-primary"></textarea>
@@ -35,7 +32,7 @@
 				class="flex w-fit items-center justify-center rounded-2xl bg-accent p-2 text-2xl font-bold"
 				onclick={createPost}
 				type="button"
-				>Post
+			>Post
 			</button>
 		</div>
 	</div>
