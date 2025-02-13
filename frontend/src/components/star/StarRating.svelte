a9d886e63cc1e5c12b6e16f1b3493a7ed15ee444
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-13 13:25:15 +0100

    frontend: posts are ratable now

--- frontend/src/components/star/StarRating.svelte
+++ frontend/src/components/star/StarRating.svelte
@@ -1,19 +1,47 @@
 <script lang="ts">
 	import Star from "$components/star/Star.svelte";
 
-	let { stars, readOnly = false }: { stars: number, readOnly?: boolean } = $props();
+	let {stars = $bindable(0.5), readOnly = true}: { stars: number, readOnly?: boolean } = $props();
 
 	// Calculate the integer and fractional parts of the score
-	const intScore: number = Math.floor(stars);
-	const fractionScore: number = stars - intScore;
+	const intScore: number = $derived(Math.floor(stars));
+	const fractionScore: number = $derived(stars - intScore);
+
+	const handleClick = (event: MouseEvent) => {
+		if (readOnly) return;
+
+		const rect = (event.currentTarget as HTMLElement).getBoundingClientRect();
+		const percentage = (event.clientX - rect.left) / rect.width;
+		stars = Math.min(5, Math.max(0.5, (percentage * 4.5) + 0.5));
+	}
 </script>
 
-{#each Array(5) as _, id}
-	{#if id === intScore}
-		<Star readOnly={readOnly} fillPercentage={fractionScore} />
-	{:else if id < intScore}
-		<Star readOnly={readOnly} fillPercentage={1} />
-	{:else}
-		<Star readOnly={readOnly} fillPercentage={0} />
-	{/if}
-{/each}
+<button class="relative" class:cursor-default={readOnly} onclick={handleClick}>
+    <div class="flex flex-row">
+        {#each Array(5) as _, id}
+            {#if id === intScore}
+                <Star readOnly={readOnly} fillPercentage={fractionScore}/>
+            {:else if id < intScore}
+                <Star readOnly={readOnly} fillPercentage={1}/>
+            {:else}
+                <Star readOnly={readOnly} fillPercentage={0}/>
+            {/if}
+        {/each}
+    </div>
+
+    {#if !readOnly}
+        <input class="slider" type="range" min={0.5} max={5} step={0.5} bind:value={stars} aria-label="Star rating"/>
+    {/if}
+</button>
+
+<style>
+    .slider {
+        opacity: 0;
+        cursor: pointer;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        height: 100%;
+    }
+</style>
--- frontend/src/routes/post/[postId]/+page.svelte
+++ frontend/src/routes/post/[postId]/+page.svelte
@@ -1,14 +1,44 @@
 <script lang="ts">
-	import type { PageProps } from "./$types";
+	import type {PageProps} from "./$types";
 	import Post from "$components/Post.svelte";
+	import StarRating from "$components/star/StarRating.svelte";
+	import {page} from "$app/state";
+	import apiClient from "$lib/ApiClient";
+	import {invalidateAll} from "$app/navigation";
 
-	let { data }: PageProps = $props();
+	let {data}: PageProps = $props();
+
+	let stars = $state<number>(0.5);
+	let value = $state<string>("");
+
+	const rate = async () => {
+		if (!stars) return;
+		if (stars < 0.5 || stars > 5) return;
+		if (!value) return;
+
+		await apiClient.ratings.v1RatingsCreate({
+            postId: page.params.postId,
+            stars: stars,
+            message: value
+        });
+		await invalidateAll();
+    }
 </script>
 
-<div class="flex flex-col divide-y divide-gray-500">
-	<Post post={data.post} />
+<div class="flex flex-col gap-y-2">
+    <Post post={data.post}/>
 
-	{#each data.ratings as rating}
-		<span>{rating.stars}</span>
-	{/each}
+    <div class="flex flex-col gap-y-2 border rounded-lg p-2.5">
+        <input bind:value type="text" placeholder="Rate with a message" class="bg-background py-4 w-full px-2 focus:outline-none"/>
+        <div class="flex flex-row justify-between items-center">
+            <StarRating bind:stars readOnly={false} />
+            <button class="bg-secondary rounded-lg px-4 py-2" onclick={rate}>
+                Rate
+            </button>
+        </div>
+    </div>
+
+    {#each data.ratings as rating}
+        <span>{rating.stars}</span>
+    {/each}
 </div>
