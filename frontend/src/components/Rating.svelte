46c93a1374b0a7a70782cce3e78cc4b6b5a61f5c
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-13 13:52:05 +0100

    frontend: add ratings visualization

---
+++ frontend/src/components/Rating.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type {RatingOutDto} from "$lib/api/Api";
+	import StarRating from "$components/star/StarRating.svelte";
+
+	let {rating}: { rating: RatingOutDto } = $props();
+</script>
+
+<div>
+    <div class="flex flex-row justify-between">
+        <span class="text-white/70 text-lg">{rating?.creatorName}</span>
+        <StarRating stars={rating?.stars}/>
+    </div>
+
+    <span>{rating?.message}</span>
+</div>
--- frontend/src/routes/post/[postId]/+page.svelte
+++ frontend/src/routes/post/[postId]/+page.svelte
@@ -5,6 +5,7 @@
 	import {page} from "$app/state";
 	import apiClient from "$lib/ApiClient";
 	import {invalidateAll} from "$app/navigation";
+	import Rating from "$components/Rating.svelte";
 
 	let {data}: PageProps = $props();
 
@@ -17,28 +18,39 @@
 		if (!value) return;
 
 		await apiClient.ratings.v1RatingsCreate({
-            postId: page.params.postId,
-            stars: stars,
-            message: value
-        });
+			postId: page.params.postId,
+			stars: stars,
+			message: value
+		});
 		await invalidateAll();
-    }
+	}
 </script>
 
 <div class="flex flex-col gap-y-2">
     <Post post={data.post}/>
 
-    <div class="flex flex-col gap-y-2 border rounded-lg p-2.5">
-        <input bind:value type="text" placeholder="Rate with a message" class="bg-background py-4 w-full px-2 focus:outline-none"/>
+    <span>Rate this post below:</span>
+    <div class="flex flex-col border rounded-lg px-2.5 pb-2 flex-wrap">
+        <textarea
+                bind:value
+                placeholder="Rate with a message"
+                class="bg-background py-4 w-full px-2 focus:outline-none
+         break-words whitespace-pre-wrap overflow-y-auto resize-none"
+        ></textarea>
         <div class="flex flex-row justify-between items-center">
-            <StarRating bind:stars readOnly={false} />
+            <StarRating bind:stars readOnly={false}/>
             <button class="bg-secondary rounded-lg px-4 py-2" onclick={rate}>
                 Rate
             </button>
         </div>
     </div>
 
-    {#each data.ratings as rating}
-        <span>{rating.stars}</span>
-    {/each}
+    {#if data.ratings.length > 0}
+        <div class="flex flex-col divide-y-2 gap-y-2">
+            <span>View other comments:</span>
+            {#each data.ratings as rating}
+                <Rating rating={rating}/>
+            {/each}
+        </div>
+    {/if}
 </div>
