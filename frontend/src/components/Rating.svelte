27c889693dd0dbb3545a7998e61bd94f3d276121
Christian Knapp <christian.alex.knapp@icloud.com>
2025-02-14 09:23:57 +0100

    frontend: add delete rating possibility

--- frontend/src/components/Rating.svelte
+++ frontend/src/components/Rating.svelte
@@ -1,15 +1,34 @@
 <script lang="ts">
-	import type {RatingOutDto} from "$lib/api/Api";
+	import type { RatingOutDto, UserOutDto } from "$lib/api/Api";
 	import StarRating from "$components/star/StarRating.svelte";
+	import { getContext } from "svelte";
+	import { Trash2 } from "lucide-svelte";
+	import apiClient from "$lib/ApiClient";
+	import { invalidateAll } from "$app/navigation";
 
-	let {rating}: { rating: RatingOutDto } = $props();
+	let { rating }: { rating: RatingOutDto } = $props();
+
+	const userData = getContext<{ currentUser: UserOutDto }>("userData");
+
+	const deleteRating = async (ratingId: string) => {
+		await apiClient.ratings.v1RatingsDelete({ ratingId: ratingId });
+		await invalidateAll();
+	}
 </script>
 
 <div>
-    <div class="flex flex-row justify-between">
-        <span class="text-white/70 text-lg">{rating?.creatorName}</span>
-        <StarRating stars={rating?.stars ?? 0}/>
-    </div>
+	<div class="flex flex-row justify-between">
+		<span class="text-white/70 text-lg">{rating?.creatorName}</span>
+		<div class="flex flex-row gap-x-2 items-center">
+			<StarRating stars={rating?.stars ?? 0} />
+			{#if userData?.currentUser.id === rating.creatorId}
+				<Trash2 color="red" class="cursor-pointer" onclick={() => {
+					if (!rating.id) return;
+					deleteRating(rating.id);
+				}} />
+			{/if}
+		</div>
+	</div>
 
-    <span>{rating?.message}</span>
+	<span>{rating?.message}</span>
 </div>
